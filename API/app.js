209c44b6f842eaf725c40e5c8c09f914392744a1
Govind Thakur <govindthakur9540@gmail.com>
2021-04-07 08:55:58 +0530

    Completed

---
+++ API/app.js
@@ -0,0 +1,23 @@
+var apikey = '5ff5de168cdc997cceb7b7fdb8331c8d';
+var msg = document.getElementById("msg");
+
+function getData(){
+    var input = document.getElementById("city").value;
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apikey}&units = metric`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById("p1").innerHTML= "Temp : "+data.main.temp;
+        document.getElementById("p2").innerHTML ="Temp : "+data.name;
+        var icon = "https://api.openweathermap.org/img/w/"+data.weather[0].icon+".png";
+        document.getElementById("icon").src = icon;
+
+    })
+    .catch(() =>{
+        msg.textContent = "Please search for a valid city";
+    });
+
+}
